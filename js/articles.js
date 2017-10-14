d6252c72ab8d88f4a3156c811a0933b353dc1bf6
Alicia Sykes <sykes.alicia@gmail.com>
2017-10-14 21:44:51 +0100

    Removed old files from master

--- js/articles.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-	$('.read-more-btn').click(function() {
-		$(this).prev().toggleClass( "showing", 1000, "easeOutSine" );
-		var text = $(this).text();
-		$(this).text(text == "Read More" ? "Show Less" : "Read More");
-	});	
-});
